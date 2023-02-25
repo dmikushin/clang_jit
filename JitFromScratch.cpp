c860f0f79cb38cfdd3c4b36f646dd0c104fd0382
Dmitry Mikushin <dmitry@kernelgen.org>
2023-02-25 12:44:22 +0100

    [WIP] Updating only the CC1 compilation for LLVM 12

--- JitFromScratch.cpp
+++ JitFromScratch.cpp
@@ -1,4 +1,5 @@
-#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include "ClangCC1Driver.h"
+
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
@@ -10,8 +11,6 @@
 #include <llvm/Support/Signals.h>
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/Support/raw_ostream.h>
-
-#include "SimpleOrcJit.h"
 
 // Show the error message and exit.
 LLVM_ATTRIBUTE_NORETURN static void fatalError(llvm::Error E) {
@@ -25,26 +24,6 @@ LLVM_ATTRIBUTE_NORETURN static void fatalError(llvm::Error E) {
   exit(1);
 }
 
-// Determine the size of a C array at compile-time.
-template <typename T, size_t sizeOfArray>
-constexpr unsigned arrayElements(T (&)[sizeOfArray]) {
-  return sizeOfArray;
-}
-
-// This function will be called from JITed code.
-DECL_JIT_ACCESS_CPP int *customIntAllocator(unsigned items) {
-  static int memory[100];
-  static unsigned allocIdx = 0;
-
-  if (allocIdx + items > arrayElements(memory))
-    exit(-1);
-
-  int *block = memory + allocIdx;
-  allocIdx += items;
-
-  return block;
-}
-
 int main(int argc, char **argv) {
   using namespace llvm;
 
@@ -53,18 +32,8 @@ int main(int argc, char **argv) {
 
   atexit(llvm_shutdown);
 
-  InitializeNativeTarget();
-  InitializeNativeTargetAsmPrinter();
-  InitializeNativeTargetAsmParser();
-
   // Parse -debug and -debug-only options.
   cl::ParseCommandLineOptions(argc, argv, "JitFromScratch example project\n");
-
-  int x[]{0, 1, 2};
-  int y[]{3, 1, -1};
-
-  auto targetMachine = EngineBuilder().selectTarget();
-  auto jit = std::make_unique<SimpleOrcJit>(*targetMachine);
 
   // Implementation of the integerDistances function.
   std::string sourceCode =
@@ -83,24 +52,13 @@ int main(int argc, char **argv) {
 
   // Compile C++ to bitcode.
   LLVMContext context;
-  auto module = jit->compileModuleFromCpp(sourceCode, context);
+  ClangCC1Driver driver;
+  auto module = driver.compileTranslationUnit(sourceCode, context);
   if (!module)
     fatalError(module.takeError());
 
-  // Compile to machine code and link.
-  jit->submitModule(std::move(*module));
-  auto jitedFn =
-      jit->getFunction<int *(int *, int *, unsigned)>("integerDistances");
-  if (!jitedFn)
-    fatalError(jitedFn.takeError());
-
-  // Invoke JITed function.
-  auto integerDistances = *jitedFn;
-  int *z = integerDistances(x, y, arrayElements(x));
-
-  outs() << "Integer Distances: ";
-  outs() << z[0] << ", " << z[1] << ", " << z[2] << "\n\n";
-  outs().flush();
+  //outs() << module.get() << "\n";
 
   return 0;
 }
+
